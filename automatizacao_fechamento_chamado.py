c1ad8a35c8e17f47fa0d0e1dcd22d9fc31f078f5
sadak2578 <108498678+sadak2578@users.noreply.github.com>
2025-01-07 15:04:20 -0300

    Add files via upload

---
+++ automatizacao_fechamento_chamado.py
@@ -0,0 +1,18 @@
+
+username = input('Nome completo da pessoa: ')
+user_in_ad = input('Coloque o nome do usuário AD aqui: ')
+admin = 'Samuel Fernandes - conv_stefanini23'
+
+print(f'''
+
+
+O funcionário foi cadastrado na rede conforme solicitado.
+ 
+Login: {user_in_ad} 
+Favor orientar o {username.title()} para ligar no HelpDesk (35892211) para a configuração do primeiro acesso.
+ 
+Qualquer dúvida, coloco-me à disposição.
+Atenciosamente.
+{admin}
+''')
+
